d63a634c7df04e754013321727ee37d8511d3992
suyeon <abwlr901@gmail.com>
2025-12-12 01:27:45 +0900

    css 일부추가+오류 발생

--- src/Login.jsx
+++ src/Login.jsx
@@ -14,53 +14,60 @@ const Login = () => {
   } = useLogin();
 
   return (
-    <div className="page">
-      <div className="titleWrap">
+    <div className="login-page">
+      <div className="title-wrap">
         <br />
-        로그인 바부는 성욱이
+        <h2>LOG IN</h2>
       </div>
-      <div className="contentWrap">
-        <div className="inputTitle">이메일 주소</div>
-        <div className="inputWrap">
+      <div className="content-wrap" role="form">
+        <div className="input-title">이메일 주소</div>
+        <div className="input-wrap">
           <input
             type="text"
-            className="input"
+            className="input-txt"
             placeholder="you@example.com"
             value={email}
             onChange={handleEmail}
           />
-        </div>
-        <div className="errorMessageWrap">
-          {!emailValid && email.length > 0 && (
-            <div>올바른 이메일을 입력해주세요.</div>
-          )}
-        </div>
-        <div style={{ marginTop: "26px" }} className="inputTitle">
-          비밀번호
-        </div>
-        <div className="inputWrap">
           <input
             type="password"
-            className="input"
+            className="input-txt"
             placeholder="영문, 숫자, 특수문자 포함 8자 이상"
             value={pw}
             onChange={handlePw}
           />
         </div>
-        <div className="errorMessageWrap">
+        <div className="error-m-wrap">
+          {!emailValid && email.length > 0 && (
+            <div>올바른 이메일을 입력해주세요.</div>
+          )}
+        </div>
+        <div style={{ marginTop: "26px" }} className="input-title">
+          비밀번호
+        </div>
+        <div className="input-wrap">
+          <input
+            type="password"
+            className="input-txt"
+            placeholder="영문, 숫자, 특수문자 포함 8자 이상"
+            value={pw}
+            onChange={handlePw}
+          />
+        </div>
+        <div className="error-m-wrap">
           {!pwValid && pw.length > 0 && (
             <div>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요. </div>
           )}
         </div>
       </div>
-      <div className="buttonWrap">
+      <div className="button-wrap">
         <button onClick={onClickConfirmButton} disabled={!allowValid}>
           로그인
         </button>
       </div>
       <hr shade="true" />
-      <div className="registerWrap">
-        <div className="registerTitle">
+      <div className="register-wrap">
+        <div className="register-title">
           계정이 없으신가요? <Link to="/register">가입하기</Link>
         </div>
       </div>
